7f38ed3be17b00b4ab1dec23ef4bd9e43a06a1cc
Sunny Dubey <sunny@audiowiki.org>
2014-02-24 17:06:35 -0500

    Add a bunch of stuff

--- lib/gem_makr/files.rb
+++ lib/gem_makr/files.rb
@@ -14,6 +14,21 @@ module Gem_Makr
         bin = ERB.new(File.read(File.join(File.dirname(__FILE__), '..', 'templates', 'bin.erb')))
         file.write(bin.result(binding))
       }
+
+      File.open(File.join(gem_name, 'lib', gem_name + '.rb'), 'w') { |file|
+        lib = ERB.new(File.read(File.join(File.dirname(__FILE__), '..', 'templates', 'lib.erb')))
+        file.write(lib.result(binding))
+      }
+
+      File.open(File.join(gem_name, 'lib', gem_name, 'application.rb'), 'w') { |file|
+        app = ERB.new(File.read(File.join(File.dirname(__FILE__), '..', 'templates', 'application.rb.erb')))
+        file.write(app.result(binding))
+      }
+
+      File.open(File.join(gem_name, 'lib', gem_name, 'options.rb'), 'w') { |file|
+        options = ERB.new(File.read(File.join(File.dirname(__FILE__), '..', 'templates', 'options.rb.erb')))
+        file.write(options.result(binding))
+      }
     rescue Exception => e
       puts "We had an issue!"
       raise e.message
